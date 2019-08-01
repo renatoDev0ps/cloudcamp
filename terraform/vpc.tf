15256466d0a7fbdd2d3592d4eafa93f2b6ffad0d
renatodevops <renatodevops@gmail.com>
2019-08-01 20:03:52 -0300

    insert route-table name

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -41,6 +41,10 @@ resource "aws_route_table" "rtb_public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.igw.id}"
   }
+
+  tags = {
+    Name = "RT-CLOUDURBANS"
+  }
 }
 
 resource "aws_route_table_association" "rta_subnet_public_1a" {
